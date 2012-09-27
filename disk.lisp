d15f6c47781f1a4eaa5eef2f31a599e12e0552c0
Stas Boukarev <stassats@gmail.com>
2012-09-28 00:56:07 +0400

    Check for circular references when saving objects recursively.

--- disk.lisp
+++ disk.lisp
@@ -585,23 +585,28 @@
 ;;; storable-object
 
 (defun write-objects-inside-slots (object stream)
-  (flet ((write-changed (x)
-           (cond ((not (typep x 'storable-object)))
-                 ((written x)
-                  (write-objects-inside-slots x stream))
-                 (t
-                  (write-storable-object x stream)))))
-    (loop for (slot-loc) across (slot-locations-and-initforms (class-of object))
-          for value = (standard-instance-access object slot-loc)
-          do
-          (cond ((consp value)
-                 (loop for (x) on value
-                       do (write-changed x)))
-                ((and (vectorp value)
-                      (not (stringp value)))
-                 (loop for x across value
-                       do (write-changed x)))
-                (t (write-changed value))))))
+  (let ((visited (make-hash-table :test #'eq)))
+    (labels ((write-changed (x)
+               (cond ((not (typep x 'storable-object)))
+                     ((gethash x visited))
+                     ((written x)
+                      (setf (gethash x visited) t)
+                      (recurse x))
+                     (t
+                      (write-storable-object x stream))))
+             (recurse (object)
+               (loop for (slot-loc) across (slot-locations-and-initforms (class-of object))
+                     for value = (standard-instance-access object slot-loc)
+                     do
+                     (cond ((consp value)
+                            (loop for (x) on value
+                                  do (write-changed x)))
+                           ((and (vectorp value)
+                                 (not (stringp value)))
+                            (loop for x across value
+                                  do (write-changed x)))
+                           (t (write-changed value))))))
+      (recurse object))))
 
 (defmethod write-object ((object storable-object) stream)
   (cond ((written object)
